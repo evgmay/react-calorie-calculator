fd5c2400a0838ce6e511bfd24df11c6399a49032
evgmay <e.dakinov@gmail.com>
2021-12-14 00:05:10 +0300

    fix

--- src/components/Mifflin-St. Jeor.js
+++ src/components/Mifflin-St. Jeor.js
@@ -1,7 +1,14 @@
-import React from "react"
-import { Container } from "react-bootstrap"
+import React, { useState } from "react"
+import { Container, Button, Offcanvas, Image } from "react-bootstrap"
+import Graph from './img/Info/Mifflin-St Jeor.png'
 
 export default function MifflinStJeor() {
+
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
     return (
         <>
             <div className="bg-digital">
@@ -11,25 +18,30 @@ export default function MifflinStJeor() {
             </div>
 
             <Container>
-                <h4 className="py-5">
-                    The Mifflin-Saint Geor formula, developed by a group of American dietitians led by Dr.
+                <h4 className="py-4">
+                    <span className="text-primary">The Mifflin-Saint Geor</span> formula, developed by a group of American dietitians led by Dr.
                     Mifflin and Saint Geor, provides the required amount of kilocalories (kcal) per day for each
                     individual person. It was introduced in 2005 and is increasingly replacing the classic
                     Harris-Benedict formula.
                 </h4>
-                <h4 className="pb-5">
+                <Image
+                    src={Graph}
+                    fluid
+                    className="pb-4"
+                />
+                <h4 className="pb-4">
                     There are two variants of the formula:
                     1. A simplified version of the Mifflin-San Geor formula:
                     For men: 10 x weight (kg) + 6.25 x height (cm) - 5 x age (g) + 5;
                     For women: 10 x weight (kg) + 6.25 x height (cm) - 5 x age (g) - 161.
                 </h4>
-                <h4 className="pb-5">
+                <h4 className="pb-4">
                     2. The modified version of the Mifflin-San Geor formula, in contrast to the simplified one, gives more
                     accurate information and takes into account the degree of physical activity of a person:<br />
                     For men: (10 x weight (kg) + 6.25 x height (cm) - 5 x age (g) + 5) x A;<br />
                     For women: (10 x weight (kg) + 6.25 x height (cm) - 5 x age (g) - 161) x A.<br />
                 </h4>
-                <h4 className="pb-5">
+                <h4 className="pb-4">
                     A is the level of human activity, it is usually distinguished by five degrees of physical activity per
                     day:<br />
                     1.2 - minimal activity, sedentary work that does not require significant physical exertion;<br />
@@ -46,6 +58,21 @@ export default function MifflinStJeor() {
                     shoveling coal or a long working day on an assembly line. This level of activity is often very difficult
                     to achieve. It should be noted that this formula is relevant only for persons aged 13 to 80 years.
                 </h4>
+
+                <Button variant="primary" onClick={handleShow} className="mb-4">More Information</Button>
+
+                <Offcanvas show={show} onHide={handleClose}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Mifflin-Saint Jeor</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <h4 className="fw-bolder mx-auto pb-3">
+                            We have collected for you easy-to-understand information about this topic.
+                            If you want to know more, we advise you to visit Wikipedia or other information sites.
+                        </h4>
+                        <Button className="outline-primary mx-auto" href="https://en.wikipedia.org/wiki/Basal_metabolic_rate">Wiki</Button>
+                    </Offcanvas.Body>
+                </Offcanvas>
             </Container>
         </>
     );
